5c6a2151719aa9bdf20c1f095e243e10c3932c3c
Kartik Poojari <poojarikartik109@gmail.com>
2021-11-10 17:50:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import json
 
-testWords = open("racial.txt", "r") #Reading and storing Sample Words
-words = testWords.read().splitlines() 
-testWords.close()
+#Reading Data
+with open("racial.txt", "r") as testWords: 
+    words = testWords.read().splitlines() 
 
-
-testTweets = open("tweets.json", "r") #Reading and storing Sample Tweets
-tweets = json.load(testTweets)
-testTweets.close()
-
+with open("tweets.json", "r") as testTweets:
+    tweets = json.load(testTweets)
 
 Tweets = []
 
+#Detection of Profane words(racial slurs)
 for j in tweets['sampleTweets']:
     profaneWordCount = (sum(j.lower().count(i) for i in words))
     Tweets.append([j, profaneWordCount])
 
+#Determining the Degree of Profanity of each tweet
+#Degree was determined on the basis of ratio of Profane Words to the Total Number of Words in the tweet.
 for i in Tweets:
-    print(i[0], (i[1]) / len(i[0]))
+    rawScore = (i[1]) / len(i[0])
+    if rawScore == 0.0:
+        degree = "NOT PROFANE"
+    elif rawScore > 0.0 and rawScore < 0.025:
+        degree = "PROFANE"
+    elif rawScore > 0.025 and rawScore < 0.05:
+        degree = "VERY PROFANE"
+    else:
+        degree = "EXTREMELY PROFANE"
+        
+    print(i[0], "-" , degree, "(", rawScore, ")", "\n")
